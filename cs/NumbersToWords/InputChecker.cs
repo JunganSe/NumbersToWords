382cf860b5593071cf40a02a5676e761f01dd10e
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2026-02-28 22:55:13 +0100

    Adjust namespace

--- cs/NumbersToWords/InputChecker.cs
+++ cs/NumbersToWords/InputChecker.cs
@@ -1,18 +1,17 @@
-﻿namespace NumbersToWords
-{
-    public class InputChecker
-    {
-        public static bool IsDigits(string? input)
-        {
-            return !string.IsNullOrEmpty(input)
-                && input.All(char.IsDigit);
-        }
+﻿namespace NumbersToWords;
 
-        public static bool IsT9(string? input)
-        {
-            string validCharacters = "23456789";
-            return !string.IsNullOrEmpty(input)
-                && input.All(c => validCharacters.Contains(c));
-        }
+public class InputChecker
+{
+    public static bool IsDigits(string? input)
+    {
+        return !string.IsNullOrEmpty(input)
+            && input.All(char.IsDigit);
+    }
+
+    public static bool IsT9(string? input)
+    {
+        string validCharacters = "23456789";
+        return !string.IsNullOrEmpty(input)
+            && input.All(c => validCharacters.Contains(c));
     }
 }
--- cs/NumbersToWords/Program.cs
+++ cs/NumbersToWords/Program.cs
@@ -1,18 +1,26 @@
-﻿Console.Write("Enter numbers: ");
-string? input = Console.ReadLine();
+﻿namespace NumbersToWords;
 
-if (NumbersToWords.InputChecker.IsDigits(input))
+internal class Program
 {
-    var wordMaker = new NumbersToWords.Wordmaker();
-    var combinations = wordMaker.GetCombinations(input!, wordMaker.KeyT9EnglishExtended);
-
-    foreach (var item in combinations)
+    private static void Main(string[] args)
     {
-        Console.WriteLine(item);
+        Console.Write("Enter numbers: ");
+        string? input = Console.ReadLine();
+
+        if (InputChecker.IsDigits(input))
+        {
+            var wordMaker = new Wordmaker();
+            var combinations = wordMaker.GetCombinations(input!, wordMaker.KeyT9EnglishExtended);
+
+            foreach (var item in combinations)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(combinations.Count + " combinations");
+        }
+        else
+        {
+            Console.WriteLine("Not T9.");
+        }
     }
-    Console.WriteLine(combinations.Count + " combinations");
-}
-else
-{
-    Console.WriteLine("Not T9.");
 }
