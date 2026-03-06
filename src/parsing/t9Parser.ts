e192d803e6470ca35e05b2b58cba65172da2379b
Johan Ljungberg <89130153+JunganSe@users.noreply.github.com>
2026-03-06 14:33:52 +0100

    Remove old parse versions

--- src/parsing/t9Parser.ts
+++ src/parsing/t9Parser.ts
@@ -1,14 +1,13 @@
-import { T9Map, T9Maps } from "@/parsing/t9Maps";
+import { T9Map } from "@/parsing/t9Maps";
 
 /** Parses T9 digits into all possible letter combinations. */
 export class T9Parser {
     /** Returns all possible letter combinations for the given T9 input and mapping. */
-    static getAllCombinations_v3(input: string, t9Map: T9Map): string[] {
+    static getAllCombinations(input: string, t9Map: T9Map): string[] {
+        let outputCombinations: string[] = [''];
         const inputDigits = [...input]
             .map(d => parseInt(d))
             .filter(d => !isNaN(d));
-
-        let outputCombinations: string[] = [''];
 
         for (const inputDigit of inputDigits) {
             const mappedLetters = this.getLettersForDigit(inputDigit, t9Map);
@@ -32,56 +31,5 @@ export class T9Parser {
             }
         }
         return newCombinations;
-    }
-
-
-    /** Returns all possible letter combinations for the given T9 input and mapping. */
-    static getAllCombinations_v2(input: string, t9Map: T9Map): string[] {
-        const inputDigits = [...input]
-            .map(d => parseInt(d))
-            .filter(d => !isNaN(d));
-
-        let outputCombinations: string[] = [''];
-
-        for (const inputDigit of inputDigits) {
-            const key = `key${inputDigit}`;
-            const mappedLetters = t9Map[key as keyof T9Map];
-            const newCombinations: string[] = [];
-
-            for (const outputCombination of outputCombinations) {
-                for (const mappedLetter of mappedLetters) {
-                    newCombinations.push(outputCombination + mappedLetter);
-                }
-            }
-            outputCombinations = newCombinations;
-        }
-        return outputCombinations;
-    }
-
-
-
-    /** Returns all possible letter combinations for the given T9 input and mapping. */
-    static getAllCombinations_v1(digits: string): string[] { // Made by copilot.
-        console.log('-- Starting parse --');
-        const map = T9Maps.t9;
-
-        const combinations: string[] = [''];
-        for (const digit of digits) {
-            console.log('Parsing digit:', digit);
-
-            const key = `key${digit}`;
-            const letters = map[key as keyof T9Map];
-            const newCombinations: string[] = [];
-            for (const combination of combinations) {
-                for (const letter of letters) {
-                    newCombinations.push(combination + letter);
-                }
-            }
-            combinations.length = 0;
-            combinations.push(...newCombinations);
-        }
-
-        console.log('-- Parse complete. --');
-        return combinations;
     }
 }
